074a4b7e0221dfee6fa310505b9c3aaf40309747
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2023-09-05 14:07:38 -0700

    execute

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+
+for ch in range(97, 123):
+    if ch == 101 or ch == 113:
+        continue
+    print("{:c}".format(ch), end="")
