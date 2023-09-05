37f164c0a4b01e93ffa0399d2d4f3705f9ba3b52
Stanley Ibeneme <stanleyibeneme92@gmail.com>
2023-09-05 14:17:22 -0700

    correct 4-6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    print('{:02d}, '.format(num), end='')
-print('99')
+
+for num in range(0, 100):
+    if num == 99:
+        print("{0:02d}".format(num))
+    else:
+        print("{0:02d},".format(num), end=" ")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
+
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
         else:
-            print('{}{}, '.format(x, y), end='')
+            print("{:d}{:d}".format(i, j), end=", ")
